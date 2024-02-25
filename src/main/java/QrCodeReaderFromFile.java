32c37eae30ad846f9d9b6ee19bf8ed8b6e1f64a7
Rishi Raj <rishiraj88+github@gmail.com>
2024-02-26 00:03:03 +0100

    Add maven nature.

--- src/main/java/QrCodeReaderFromFile.java
+++ src/main/java/QrCodeReaderFromFile.java
@@ -1,10 +1,13 @@
+import com.google.zxing.MultiFormatReader;
+
 import javax.imageio.*;
 import java.io.File;
 
 public class QrCodeReaderFromFile {
     Object read(String filePath) {
         File file = new File(filePath);
-        new MultiFormatReader()
+        git status new MultiFormatReader()
+
 
     }
 }
