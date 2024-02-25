74b8392d149d7cdbb78953529b1a28004e09e20c
Rishi Raj <rishiraj88+github@gmail.com>
2024-02-25 23:55:21 +0100

    Add QrGenerator.

---
+++ QrCodeReaderFromFile.java
@@ -0,0 +1,10 @@
+import javax.imageio.*;
+import java.io.File;
+
+public class QrCodeReaderFromFile {
+    Object read(String filePath) {
+        File file = new File(filePath);
+        new MultiFormatReader()
+
+    }
+}
---
+++ QrGenerator.java
@@ -0,0 +1,9 @@
+public class QrGenerator {
+    public static void main(String[] args) {
+
+        // Create QR code and export to file.
+
+        // Read QR code from file and decode information out of it
+    }
+
+}
